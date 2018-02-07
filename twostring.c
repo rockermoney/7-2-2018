c96d2b0bc023b473b1a23173ccb0336658a56857
rockermoney <35295528+rockermoney@users.noreply.github.com>
2018-02-07 16:36:10 +0530

    Create twostring.c

---
+++ twostring.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char a[10];	char b[10];
+printf("enter string 1\n");
+scanf(#"%s",&a);
+printf("enter string 2\n");
+scanf("%s",&b);
+printf("concatenated strings:%s%s",a,b);
+}
