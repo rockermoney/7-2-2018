5cfba868a401eca87f7f37f76990eae262253946
rockermoney <35295528+rockermoney@users.noreply.github.com>
2018-02-07 16:22:56 +0530

    Create swap.c

---
+++ swap.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{int j,k,t;
+printf("\n initialise the values");
+printf("\n enter the first number");
+scanf("%d",&j);
+printf("\n enter the second number");
+scanf("%d",&k);t=j;j=k;k=t;
+printf("\n after swapping %d %d",j,k);
+return 0;
+}
