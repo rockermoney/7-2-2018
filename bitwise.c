26bc2fce1b4ccb3c8dfc2f44a80d64cd496df7f4
rockermoney <35295528+rockermoney@users.noreply.github.com>
2018-02-07 16:25:13 +0530

    Create bitwise.c

---
+++ bitwise.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<conio.h.
+int main()
+{int m,n;
+printf("enter two numbers");
+scanf("%d%d",&m,&n);m=m*n;n=m*n;m=m*n;
+printf("\n the swapping is %d %d",m,n);
+return(0);
+}
